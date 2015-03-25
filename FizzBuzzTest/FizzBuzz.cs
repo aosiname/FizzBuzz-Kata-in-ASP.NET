37e5ccc58c2759544eaa95313d8e970bee5cc764
aosiname <osinamea@gmail.com>
2015-03-25 15:38:06 +0000

    initial commit

---
+++ FizzBuzzTest/FizzBuzz.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FizzBuzzTest
+{
+    class FizzBuzz
+    {
+        internal string evaluate(int p)
+        {
+            if (isMultipleOfThree(p) && isMultipleOfFive(p))
+            {
+                return "FizzBuzz";
+            }
+            else if (isMultipleOfThree(p))
+            {
+                return "Fizz";
+            }
+            else if (isMultipleOfFive(p))
+            {
+                return "Buzz";
+            }
+            else
+            {
+                return p.ToString();
+            }
+        }
+
+        public bool isMultipleOfThree(int num)
+        {
+            return num % 3 == 0;
+        }
+
+        public bool isMultipleOfFive(int num)
+        {
+            return num % 5 == 0;
+        }
+
+        #region NOT USED
+
+        public bool isMultipleOfThreeAndFive(int num)
+        {
+            return isMultipleOfThree(num) && isMultipleOfFive(num);
+        }
+
+        /// <summary>
+        /// Remove the factor from the calculator and pass it in by inverting the control to the caller
+        /// </summary>
+        /// <param name="numberToTest"></param>
+        /// <param name="factor"></param>
+        /// <returns></returns>
+        public bool isMultipleOf(int numberToTest, int factor)
+        {
+            return numberToTest % factor == 0;
+        }
+
+        #endregion
+    }
+}
---
+++ FizzBuzzTest/FizzBuzzTest.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FizzBuzzTest
+{
+    [TestClass]
+    public class FizzBuzzTest
+    {
+        private FizzBuzz fb;
+        private string result;
+
+        
+        [TestInitialize]
+        public void canCreateClass()
+        {
+            fb = new FizzBuzz();
+            result = "";
+        }
+
+        [TestMethod]
+        public void canEvaluate()
+        {
+            result = fb.evaluate(1);
+        }
+
+        [TestMethod]
+        public void oneReturnsOne()
+        {
+            result = fb.evaluate(1);
+            Assert.AreEqual(result, "1");
+        }
+
+        [TestMethod]
+        public void twoReturnsTwo()
+        {
+            result = fb.evaluate(2);
+            Assert.AreEqual(result, "2");
+        }
+
+        [TestMethod]
+        public void threeReturnsFizz()
+        {
+            result = fb.evaluate(3);
+            Assert.AreEqual(result, "Fizz");
+        }
+
+        [TestMethod]
+        public void fiveReturnsBuzz()
+        {
+            result = fb.evaluate(5);
+            Assert.AreEqual(result, "Buzz");
+        }
+
+        [TestMethod]
+        public void sixReturnsFizz()
+        {
+            result = fb.evaluate(6);
+            Assert.AreEqual(result, "Fizz");
+        }
+
+        [TestMethod]
+        public void sevenReturnsSeven()
+        {
+            result = fb.evaluate(7);
+            Assert.AreEqual(result, "7");
+        }
+
+        [TestMethod]
+        public void nineReturnsFizz()
+        {
+            result = fb.evaluate(9);
+            Assert.AreEqual(result, "Fizz");
+        }
+
+        [TestMethod]
+        public void tenReturnsBuzz()
+        {
+            result = fb.evaluate(10);
+            Assert.AreEqual(result, "Buzz");
+        }
+
+        [TestMethod]
+        public void fifteenReturnsFizzBuzz()
+        {
+            result = fb.evaluate(15);
+            Assert.AreEqual(result, "FizzBuzz");
+        }
+
+        [TestMethod]
+        public void thirtyReturnsFizzBuzz()
+        {
+            result = fb.evaluate(30);
+            Assert.AreEqual(result, "FizzBuzz");
+        }
+
+        [TestMethod]
+        public void thirtyThreeReturnsFizz()
+        {
+            result = fb.evaluate(33);
+            Assert.AreEqual(result, "Fizz");
+        }
+
+    }
+}
